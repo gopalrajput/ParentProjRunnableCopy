ab83fdc6ede7245d167ebc8a761019e2f5bd2848
Gopal Singh Rajput <gopal.rajput@capgemini.com>
2016-08-31 14:42:44 +0530

    test Commit

---
+++ FirstProj/src/main/java/com/first/FirstPlugin.java
@@ -0,0 +1,15 @@
+package com.first;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class FirstPlugin extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public FirstPlugin() {
+		JOptionPane.showMessageDialog(this, "Calling Plugin !!!!!");
+	}
+
+}
